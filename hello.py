930b4e685ac1120b6687a4e02487a3c4281bd0a6
Yue Chen <vocal011108@gmail.com>
2026-06-20 19:21:45 -0400

    06/20/2026 new commit to refresh what i learned last time.

--- hello.py
+++ hello.py
@@ -3,3 +3,5 @@ print("Hello! This is my first Git commit. ")
 print("And this is my 2nd commit. Just getting use to it!")
 
 print("This is my 3rd line and Im attempting to save to git by myself.")
+
+print("Today is June 20 and I'm making my 4th commit. Starting with refresher.")
