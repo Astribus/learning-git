cae54ebf60fe36558da9383022e093de1bb5ee8d
Yue Chen <vocal011108@gmail.com>
2026-06-20 20:54:21 -0400

    added some bugs. Jk another practice commit

--- hello.py
+++ hello.py
@@ -5,3 +5,5 @@ print("And this is my 2nd commit. Just getting use to it!")
 print("This is my 3rd line and Im attempting to save to git by myself.")
 
 print("Today is June 20 and I'm making my 4th commit. Starting with refresher.")
+
+print("Im testing git status and seeing what terminal will say when i didnt commit yet.")
