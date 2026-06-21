da88e085ac6d346af6024f7f515ea154ab0b4ed6
Yue Chen <vocal011108@gmail.com>
2026-06-20 21:22:07 -0400

    Testing git diff and git status

--- hello.py
+++ hello.py
@@ -7,3 +7,5 @@ print("This is my 3rd line and Im attempting to save to git by myself.")
 print("Today is June 20 and I'm making my 4th commit. Starting with refresher.")
 
 print("Im testing git status and seeing what terminal will say when i didnt commit yet.")
+
+print("testing git diff")
